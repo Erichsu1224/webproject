f51c0905b1df552f2d63298314bb4caca77b47ea
Eric <40513062+Erichsu1224@users.noreply.github.com>
2019-05-10 21:06:28 +0800

    2019-5-10

--- CourseMatch/View/body/editcourse.php
+++ CourseMatch/View/body/editcourse.php
@@ -28,46 +28,81 @@
   </div>
 </div>
 
-<div>
-  <?php
-    $sql= "SELECT * from $name order BY `date`,`start_time`";
-    $result = mysqli_query($con, $sql);
+<div style="float:left; margin-top:20px;">
+  <div>
+    <?php
+      $sql= "SELECT * from $name order BY `date`,`start_time`";
+      $result = mysqli_query($con, $sql);
 
-    for($i = 0; $i < mysqli_num_rows($result); $i++)
-    {
-      $row = mysqli_fetch_row($result);
-      echo "<form method=post><input type=submit value=刪除>";
-      echo "<b>";
-      switch($row[1])
+      for($i = 0; $i < mysqli_num_rows($result); $i++)
       {
-        case 1:
-          echo "星期一 ";
-          break;
-        case 2:
-          echo "星期二 ";
-          break;
+        $row = mysqli_fetch_row($result);
+        echo "<form method=post><input type=hidden name=chk value=delete><input type=hidden name=cs value=".$row[0]."><input type=submit value=刪除>";
+        echo "<b>";
+        switch($row[1])
+        {
+          case 1:
+            echo "星期一 ";
+            break;
+          case 2:
+            echo "星期二 ";
+            break;
 
-        case 3:
-          echo "星期三 ";
-          break;
+          case 3:
+            echo "星期三 ";
+            break;
 
-        case 4:
-          echo "星期四 ";
-          break;
+          case 4:
+            echo "星期四 ";
+            break;
 
-        case 5:
-          echo "星期五 ";
-          break;
+          case 5:
+            echo "星期五 ";
+            break;
 
-        case 6:
-          echo "星期六 ";
-          break;
+          case 6:
+            echo "星期六 ";
+            break;
+        }
+
+        echo $row[2]."~".$row[3].":</b> ".$row[0];
+        echo "</form>";
       }
+     ?>
+  </div>
 
-      echo $row[2]."~".$row[3].":</b> ".$row[0];
-      echo "</form>";
-    }
+  <div style="margin-top:20px">
+    <form action="" method=post>
+      <input type=text name="course" placeholder="課程名稱">
+      <input type=text name="date" placeholder="星期幾(請輸入1~6)">
+      <input type=text name="start_time" placeholder="開始時間(xx:xx)">
+      <input type=text name="end_time" placeholder="結束時間(xx:xx)">
+      <input type=hidden name=chk value="insert">
+      <input type=submit value="新增">
+    </form>
 
-   ?>
+    <?php
+      if($_POST)
+      {
+        if($_POST['chk'] == "insert")
+        {
+          $course = $_POST['course'];
+          $date = $_POST['date'];
+          $start_time = $_POST['start_time'];
+          $end_time = $_POST['end_time'];
 
+          $sql = "INSERT INTO `$name`(`course`, `date`, `start_time`, `end_time`) VALUES('$course', $date, '$start_time', '$end_time')";
+          mysqli_query($con, $sql);
+        }
+        if($_POST['chk'] == "delete")
+        {
+          $course = $_POST['cs'];
+          $sql = "DELETE From `$name` WHERE `course` = '$course'";
+          mysqli_query($con, $sql);
+        }
+
+        header("refresh:0;url=editcourse");
+      }
+    ?>
+  </div>
 </div>
